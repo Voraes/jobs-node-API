8165d5906d1390e601cbc3b7f21a8e133c1d7dac
victor <victor.inscriptio@gmail.com>
2023-09-11 11:42:46 -0300

    Adding swaggerUI

--- app.js
+++ app.js
@@ -35,8 +35,17 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 // routes
-app.get('/', (req, res) => res.send('<h1>Jobs API</h1><br>'));
+app.get('/', (req, res) =>
+  res.send('<h1>Jobs API</h1><br><a href="/api/docs">Documentation</a>')
+);
+app.use('api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticationMiddleware, jobsRouter);
 
